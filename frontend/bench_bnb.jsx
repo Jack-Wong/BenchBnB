b53646bd7e5017aa591ce7ab28527942edf15c76
Jack-Wong <wong.jack02@gmail.com>
2016-10-18 16:32:57 -0400

    Add preloaded state and onEnter hooks to component root

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom';
 
 import configureStore from './store/store';
 import Root from './components/root';
-import { login } from './util/session_api_util';
+// import { login } from './util/session_api_util';
 
-window.store = configureStore();
-window.login = login;
-
+// window.store = configureStore();
+// window.login = login;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+  if (window.currentUser){
+    const preloadedState = {
+      session: {
+        currentUser: window.currentUser,
+        errors: []
+      }
+    };
+    var store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
